13fe43d93ec77bb5ef55b0e4698f51bc1dab543c
carloscaserotto <carlos.caserotto@yahoo.com>
2023-11-30 10:27:39 -0300

    Add User model, validation and bycryp

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    #password:string               -- virtual with has_secure_password
+    #password_confirmation:string  --virtual with has_secure_password
+    has_secure_password
+
+    validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "is not a valid email address" }
+    validates :password, presence: true
+end
